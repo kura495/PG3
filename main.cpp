cca14b0762de0df5423a801892daa310dc5f314f
kura495 <104417866+kura495@users.noreply.github.com>
2023-10-02 12:42:13 +0900

    Fix main.cpp

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@
 template <typename Type>
 //TODO:printfを関数内に入れる
 Type Min(Type a, Type b) {
-
-	return printf(, a < b ? a : b);
+	return a < b ? a : b;
 }
 
 //テンプレートの解除
 template <>
 
 char Min<char>(char a, char b) {
-	return printf("数字以外は代入できません\n");
+	printf("数字以外は代入できません\n");
+	return '\0';
 }
 
 int main() {
 	//int
-	Min<int>(1,4);
+	printf("%d\n", Min<int>(1, 4));
+	
 	//float
-	Min<float>(3.21f, 3.1f);
+	printf("%f\n", Min<float>(3.21f, 3.1f));
 	//double
-	Min<double>(5.4, 5.7);
+	printf("%lf\n", Min<double>(5.4, 5.7));
 	//char
 	Min<char>('a', 'b');
 
