29181da07125b1a4ec92e2e4c4a8e79be15d3e98
kura495 <104417866+kura495@users.noreply.github.com>
2023-10-23 13:01:29 +0900

    q

--- main.cpp
+++ main.cpp
@@ -7,10 +7,21 @@
 std::random_device seed_Gen;//非決定的な乱数生成器
 std::mt19937 mtrand(seed_Gen());//メルセンヌ・ツイスタの32bit版　引数は初期シード
 
-typedef int (*Pfunc)();//関数ポインタ
+typedef int (*Pfunc)(int);//関数ポインタ
 
-int DiceRoll(){
-	return std::uniform_int_distribution<int>(1, 6)(seed_Gen);//1~6の間の値を生成できる
+int DiceRoll(int Answer){
+	int RollResult = std::uniform_int_distribution<int>(1, 6)(seed_Gen);//1~6の間の値を生成できる
+	printf("Answer : %d\n", RollResult);
+	DiceResult(RollResult);
+	if (RollResult % 2 == 0 && Answer == 2) {
+		printf("あなたの勝ち\n");
+	}
+	else if (RollResult % 2 == 1 && Answer == 1) {
+		printf("あなたの勝ち\n");
+	}
+	else {
+		printf("あなたの負け\n");
+	}
 }
 
 void DiceResult(int diceNumber) {
@@ -25,6 +36,7 @@ void DiceResult(int diceNumber) {
 
 int SetTimeOut(Pfunc collback,int second) {
 	std::this_thread::sleep_for(std::chrono::seconds(second));
+	collback();
 	return collback();
 
 }
@@ -32,7 +44,7 @@ int SetTimeOut(Pfunc collback,int second) {
 int main() {
 
 	Pfunc p;
-	p = DiceRoll;
+	
 	int RollResult;
 	int Answer;
 
@@ -40,21 +52,12 @@ int main() {
 		
 		printf("1:半(奇数)　2:丁(偶数)　0:終了\n");
 		std::cin >> Answer;
+		p = DiceRoll(Answer);
 		if (Answer == 0) {
 			break;
 		}
 		RollResult = SetTimeOut(p, 3);
-		printf("Answer : %d\n",RollResult);
-		DiceResult(RollResult);
-		if (RollResult % 2 == 0 && Answer == 2) {
-			printf("あなたの勝ち\n");
-		}
-		else if (RollResult % 2 == 1 && Answer == 1) {
-			printf("あなたの勝ち\n");
-		}
-		else {
-			printf("あなたの負け\n");
-		}
+		
 
 	}
 
