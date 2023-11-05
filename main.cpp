0b78da5564c192c0c731993c86eabd69c527907e
kura495 <104417866+kura495@users.noreply.github.com>
2023-11-05 23:51:58 +0900

    complete task

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
-
+#include "Circle.h"
+#include "Rectangle.h"
 
 
 int main() {
-
+	//‰~
+	Circle* circle = new Circle();
+	circle->Initalize(2.0f);
+	circle->size();
+	circle->draw();
+	//‹éŒ`
+	Rectangle* rectangle = new Rectangle();
+	rectangle->Initalize(2.0f,1.0f);
+	rectangle->size();
+	rectangle->draw();
 
 
 	return 0;
