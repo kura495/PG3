e3c7083f546d81e9cbd69191d430150f05721baa
kura495 <104417866+kura495@users.noreply.github.com>
2024-01-29 11:36:34 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -2,21 +2,16 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <chrono>
 #include <thread>
 
-void DrawNumber(int number) {
-	std::string output = "thread ";
-	std::cout << output << number << std::endl;
+void StringCopy() {
+	
 }
 
 int main() {
 
-	std::thread th1(DrawNumber,1);
-	th1.join();
-	std::thread th2(DrawNumber,2);
-	th2.join();
-	std::thread th3(DrawNumber,3);
-	th3.join();
+
 
 	return 0;
 }
