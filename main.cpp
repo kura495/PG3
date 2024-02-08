7e7420400c32f2de29e552ee1cf0566f7da0020a
kura495 <104417866+kura495@users.noreply.github.com>
2024-02-08 14:01:12 +0900

    complete

--- main.cpp
+++ main.cpp
@@ -5,13 +5,20 @@
 #include <chrono>
 #include <thread>
 
-void StringCopy() {
-	
-}
-
 int main() {
-
-
+	std::string a(1000000, 'a');
+    
+    auto start = std::chrono::steady_clock::now();
+    std::string copy = a;
+    auto end = std::chrono::steady_clock::now();
+    auto copytime = end - start;
+    start = std::chrono::steady_clock::now();
+    std::string move = std::move(a);
+    end = std::chrono::steady_clock::now();
+    auto movetime = end - start;
+    std::cout << "1,000,000文字を移動とコピーで比較しました" << std::endl;
+    std::cout << "コピー:" << " " << copytime.count() << "μs" << std::endl;
+    std::cout << "移動:" << " " << movetime.count() << "μs" << std::endl;
 
 	return 0;
 }
